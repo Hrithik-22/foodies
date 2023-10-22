625d32473fa507e210993a243ce65a6df624a2d4
Hrithik Kedare <kedarehrithik@gmail.com>
2023-10-22 21:33:18 +0530

    initial-commit

---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function WelcomeScreen() {
+  return (
+    // view --> div
+    //text--> line or span
+    <View className="flex-1 justify-center items-center">
+      <Text>Welcome Screen</Text>
+    </View>
+  );
+}
